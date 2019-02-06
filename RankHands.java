1f197dbaec8d5975856ef14d51f4ff9328c618b2
JordanTTran <JordanTTran@hotmail.com>
2019-02-06 16:54:09 -0700

    Update RankHands.java

--- RankHands.java
+++ RankHands.java
@@ -1,1 +1,43 @@
 
+public class RankHands {
+	
+	public void checkRoyalFlush() {
+		
+	}
+	
+	public void checkStraightFlush() {
+		
+	}
+	
+	public void checkFourOfAKind() {
+		
+	}
+	
+	public void checkFullHouse() {
+		
+	}
+	
+	public void checkFlush() {
+		
+	}
+	
+	public void checkStraight() {
+		
+	}
+	
+	public void checkThreeOfAKind() {
+		
+	}
+	
+	public void checkTwoPair() {
+		
+	}
+	
+	public void checkPair() {
+	}
+	
+	public void checkHighCard() {
+		
+	}
+
+}
