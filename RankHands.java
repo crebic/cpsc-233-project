18b4a41ab32e89862dd4c9b38c5836c452ad59eb
JordanTTran <JordanTTran@hotmail.com>
2019-02-06 17:50:06 -0700

    Update RankHands.java

--- RankHands.java
+++ RankHands.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
 
-public class RankHands 
-{
+import javax.smartcardio.Card;
 
-	//Checks to see if there are 5 matching suits, if it does then check for Flush, Straight Flush, or Royal Flush
+public class RankHands {
+	//Players hand and board cards 
+	private ArrayList<Card> allCards = new ArrayList<Card>();
+
+	//Appends the 2 hand cards and boards card into one list
+	public void createCompareList(/* boardCardsArray, playerHandArray */) 
+	{ 
+		for (Card eachCard: boardCardsArray) {
+			allCards.add(eachCard); 
+		}
+		for (Card cards: playerHandArray ) {
+			allCards.add(eachCard); 
+		}
+		
+		
+	
+	}
 	public void checkSuit() 
 	{
+		
+		for(Card eachCard: board) {
+			
+		}
+		
 	
 	}
 	public void checkRoyalFlush() 
