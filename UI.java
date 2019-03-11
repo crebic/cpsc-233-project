90dea0624650805543601f297960e2c932badd56
JordanTTran <JordanTTran@hotmail.com>
2019-03-11 14:15:25 -0600

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,348 +1,148 @@
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.*;
-import javafx.scene.shape.*;
-import javafx.scene.text.*;
+import java.util.Scanner;
+ public class gameState{
+	 
+//* instance variables
 
-//TODO gamestate methods added, event handlers that tie all buttons to gamestate methods, adapt bet to GUI
+private int pot = 0;
+private int currentPlayer;
+private int callAmount;
+private int playerTurn = 0;
+private int numberOfPlayers = 0;
+public ArrayList<Card> tableCards = new ArrayList<Card>();
+private Player[] players;
+private Player playerWithHighestCallAmount;
+private ArrayList<Integer> notWinners = new ArrayList<Integer>();
 
-public class UI extends Application {
-	private Deck deck = new Deck();//
-	private int pot = 0;
-	private int currentPlayer;
-	private int callAmount;
-	private int playerTurn = 0;
-	private int numberOfPlayers = 0;
-	public ArrayList<Card> tableCards = new ArrayList<Card>();
-	private ArrayList<Player> players = new ArrayList<Player>();
-	private Player playerWithHighestCallAmount;
-	private ArrayList<Integer> notWinners = new ArrayList<Integer>();
-	VBox root2 = new VBox();
-	HBox tableCardsBox = new HBox();
-	HBox buttonOptions = new HBox();
-	VBox playerStats = new VBox();
-	HBox playerCardsBox = new HBox();
-	int round = 0;
-
-	public static void main(String[] args) {
-		
-		launch(args);
-	}
-
-	public void start(Stage primaryStage) throws Exception{
-		primaryStage.setTitle("Texas Hold'em");
-
-		// root and scene are for main menu, root2 and scence2 are for the game state
-		Pane root = new Pane();
-		Scene scene = new Scene(root, 800, 600);
-		Scene scene2 = new Scene(root2, 800, 600);
+gameState()
+{
+}
 
 
-		root2.setSpacing(30);
-		root2.setAlignment(Pos.CENTER);
-		tableCardsBox.setAlignment(Pos.CENTER);
-
-		root.setStyle("-fx-background-color: #228B22;");
-		root2.setStyle("-fx-background-color: #228B22;");
-
-		//setting up button options for betting
-		Button raise = new Button();
-		raise.setText("Raise");
-		Button call = new Button();
-		call.setText("Call");
-		Button check = new Button();
-		check.setText("Check");
-		Button fold = new Button();
-		fold.setText("Fold");
-
-		buttonOptions.getChildren().addAll(raise,call,check,fold);
-
-		buttonOptions.setSpacing(20);
-		buttonOptions.setAlignment(Pos.CENTER);
-
-		//raise button just acting as a next round button
-		raise.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				//not doing anything with raise amount as of now
-				String raiseAmount = JOptionPane.showInputDialog("How much would you like to raise?");
-				if(round == 0)
-				{
-					//round++;
-					betRound();
-				}
-				else if(round == 1)
-				{
-					//round++;
-					betRound();
-				}
-				else if(round == 2)
-				{
-					//round++;
-					betRound();
-				}				
-			}
-		});
-		//
-
-		Button btn1 = new Button();
-		btn1.setText("Start");
-		btn1.setLayoutX(380);
-		btn1.setLayoutY(500);
-		root.getChildren().add(btn1);
-
-		TextField pickStartChipsField = new TextField("Enter Chip Amount");
-		pickStartChipsField.setPrefWidth(200);
-		pickStartChipsField.setLayoutX(200);
-		pickStartChipsField.setLayoutY(200);
-		root.getChildren().add(pickStartChipsField);
-
-		TextField pickPlayerAmount = new TextField("Enter Number of Players");
-		pickPlayerAmount.setPrefWidth(150);
-		pickPlayerAmount.setLayoutX(200);
-		pickPlayerAmount.setLayoutY(250);
-		root.getChildren().add(pickPlayerAmount);
-
-		Text gameName = new Text("Texas Hold'em");
-		gameName.setFill(Color.WHITE);
-		gameName.setFont(Font.font("Arial", FontWeight.BOLD, 60)); // gameName.setStyle("-fx-font: 50 arial;");
-		gameName.setLayoutX(175);
-		gameName.setLayoutY(100);
-		root.getChildren().add(gameName);
-
-		int amount = 0;
-		Label pot = new Label("Current Pot: " + this.pot);
-		pot.setTextFill(Color.WHITE);
-		pot.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-		root2.getChildren().add(pot);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-		btn1.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			// @Param event is the user input that occurs, in this case the button click
-			// Method deposits the specified amount when the "Deposit" button is clicked
-			public void handle(ActionEvent event) {
-				// deposit EventHandler
-
-				int chips = Integer.parseInt(pickStartChipsField.getText());
-				int players = Integer.parseInt(pickPlayerAmount.getText());
-
-				initialize(chips,players);
-				
-				root2.getChildren().add(tableCardsBox);
-				
-				root2.getChildren().add(buttonOptions);
-
-				//setting up player stats
-				Label playerStack = new Label("Your stack: " + chips);
-				playerStack.setTextFill(Color.WHITE);
-				playerStack.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-				playerStats.getChildren().add(playerStack);
-				showPlayerCards();
-				//
-				root2.getChildren().add(playerStats);
-				primaryStage.setScene(scene2);
-				primaryStage.show();
-				root.getChildren().clear();
-
-			}
-		});
-		
-		//new shit below
-		while (getPlayersWithChips() > 1){
-			deck.resetDeck();
-			deck.deal();
-			betRound();
-			callAmount = 0;
-			betRound();
-			showTableCards(3);
-
-			callAmount = 0;
-			betRound();
-			showTableCards(4);
-
-			callAmount = 0;
-			//River:
-			betRound();
-			showTableCards(5);
-
-			RankHands rank = new RankHands();
-			notWinners = getNotFoldedPlayers();
-			int winner = rank.ranking(tableCards, getNotFoldedPlayers(), notWinners);
-			players.get(winner).addChips(pot);
-			pot = 0;
-			callAmount = 0;
-			notWinners.clear();
-			//getPlayersWithChips();
-			
-			
-			
-			//Tester above
-		}
-	}
-
-	//I know this is basically copy and pasted, sue me im tired
-	void showPlayerCards()
-	{
-		Card[] playerCards = players.get(0).getPair();
-
-		for(int x = 0; x < 2; x++)
-		{
-			//load players card images
-			String card = "";
-			int cardValue = playerCards[x].getValue();
-
-			//replace number with J/Q/K
-			if(cardValue == 11)
-			{
-				card = "J" + playerCards[x].getSuit().substring(0,1).toUpperCase();
-			}
-			else if(cardValue == 12)
-			{
-				card = "Q" + playerCards[x].getSuit().substring(0,1).toUpperCase();
-			}
-			else if(cardValue == 13)
-			{
-				card = "K" + playerCards[x].getSuit().substring(0,1).toUpperCase();
-			}
-			else if(cardValue == 14)
-			{
-				card = "A" + playerCards[x].getSuit().substring(0,1).toUpperCase();
-			}
-			else
-			{
-				card = playerCards[x].getValue() + playerCards[x].getSuit().substring(0,1).toUpperCase();
-			}
-			//add images
-			Image image = new Image(card + ".png");
-			ImageView imgView = new ImageView();
-			imgView.setImage(image);
+gameState(int newPot, int newCurrentPlayer){
+	this.pot = newPot;
+	this.currentPlayer = newCurrentPlayer;
 	
-			imgView.setPreserveRatio(true);
-			imgView.setFitHeight(150);
-			imgView.setFitWidth(150);
-
-			playerCardsBox.getChildren().add(imgView);
-		}
-		playerStats.getChildren().add(playerCardsBox);
 	}
 
-	void showTableCards(int amountOfCards)
-	{
-		tableCardsBox.getChildren().clear();
-		for(int x = 0; x < amountOfCards; x++)
-		{
-			//load flop images
-			String card = "";
-			int cardValue = tableCards.get(x).getValue();
-
-			//replace number with J/Q/K
-			if(cardValue == 11)
-			{
-				card = "J" + tableCards.get(x).getSuit().substring(0,1).toUpperCase();
-			}
-			else if(cardValue == 12)
-			{
-				card = "Q" + tableCards.get(x).getSuit().substring(0,1).toUpperCase();
-			}
-			else if(cardValue == 13)
-			{
-				card = "K" + tableCards.get(x).getSuit().substring(0,1).toUpperCase();
-			}
-			else if(cardValue == 14)
-			{
-				card = "A" + tableCards.get(x).getSuit().substring(0,1).toUpperCase();
-			}
-			else
-			{
-				card = tableCards.get(x).getValue() + tableCards.get(x).getSuit().substring(0,1).toUpperCase();
-			}
-			//add images
-			Image image = new Image(card + ".png");
-			ImageView imgView = new ImageView();
-			imgView.setImage(image);
+public int getPot(){
+	return pot;
+	}
 	
-			imgView.setPreserveRatio(true);
-			imgView.setFitHeight(200);
-			imgView.setFitWidth(200);
+	
+//* for player turn use modulus of number of players to determine current players turn.
+//* method for player turn
 
-			tableCardsBox.getChildren().add(imgView);
-		}
+
+/*
+public int getCurrentPlayer(){
+	return new currentPlayer;
 	}
-	public void initialize(int startingChips, int numberOfPlayers) {
-			for(int x = 0; x < numberOfPlayers; x++) {
-				players.add(new Player(startingChips, x+1));
-			}
-			//Deck deck = new Deck();
-			Player[] playersArray = new Player[players.size()];
-			for(int x = 0; x < players.size(); x++)
+*/
+	
+public void raise(Player currentPlayer){
+	System.out.println("how much would you like to bet?");
+			Scanner scanner = new Scanner(System.in);
+			int raiseAmount = scanner.nextInt();
+		
+	if (raiseAmount <= currentPlayer.getChips()){
+				callAmount += raiseAmount; 
+				
+		currentPlayer.addChips(-raiseAmount);
+		pot += raiseAmount;
+		playerWithHighestCallAmount = currentPlayer;
+	}
+}
+
+public void fold(Player BOB){
+	BOB.changeFoldedState(true);
+}
+
+public void call(Player BOB){
+	
+	if (callAmount <= BOB.getChips()){
+		BOB.addChips(-callAmount);
+		pot += callAmount;
+	}
+	
+}
+	
+
+/*	
+public static void main (String[] args){
+	gameState gs = new gameState();
+	*/
+	
+	public void populateTable(int startingChips, int numberOfPlayers)
+	{
+			//add four bots for now, human player is first seat
+			for(int x = 0; x < numberOfPlayers; x++)
 			{
-				playersArray[x] = players.get(x);
+				players[x] = new Player(startingChips, x+1);
 			}
-			//deck.deal(playersArray, tableCards);
-
-			//getNotFoldedPlayers();
-			
-			//RankHands rank = new RankHands(); 
-			//int winner = rank.ranking(tableCards, getNotFoldedPlayers(), notWinners);
-			//System.out.println("The winner is player " +  (winner+1));
-			
-
 	}
+	
+	public void flop(){
+					System.out.println("THE FLOP IS:\n");
 
+		for(int x = 0; x < 3; x++)
+		{
+			System.out.print(tableCards.get(x).getValue() + tableCards.get(x).getSuit().substring(0,1).toUpperCase() + " ");
+		}
+		betRound();
+	}
+	public void turn(){
+		for(int x = 0; x < 4; x++)
+		{
+			System.out.print(tableCards.get(x).getValue() + tableCards.get(x).getSuit().substring(0,1).toUpperCase() + " ");
+		}
+		betRound();
+		
+		
+	}
+	public void river(){
+		for(int x = 0; x < 5; x++)
+		{
+			System.out.print(tableCards.get(x).getValue() + tableCards.get(x).getSuit().substring(0,1).toUpperCase() + " ");
+		}
+		betRound();
+		RankHands rank = new RankHands(); 
+		int winner = rank.ranking(tableCards, getNotFoldedPlayers(), notWinners);
+		System.out.println("The winner is player " +  (winner+1));
+		//Winner is printed as the correct number
+		players[winner].addChips(pot);
+		pot = 0;
+		
+	}
+	
+	
+	public int getPlayersWithChips(){
+		int playersStillPlaying = 0;
+		for (Player eachPlayer: players) {
+			if(eachPlayer.getChips() > 0) {
+				playersStillPlaying +=1;
+			}
+		}
+		return playersStillPlaying;
+	}
+	
+	/*
 	public ArrayList<Player> getNotFoldedPlayers()
 	{
 		ArrayList<Player> playersNotFolded = new ArrayList<Player>();
-		for(int x = 0; x < players.size(); x++)
+		for(int x = 0; x < players.length; x++)
 		{
-			if(!players.get(x).getDidFold())
+			if(!players[x].getDidFold())
 			{
-				playersNotFolded.add(players.get(x));
+				playersNotFolded.add(players[x]);
 			} else {
-				playersNotFolded.add(players.get(x));
+				playersNotFolded.add(players[x]);
 				notWinners.add(x);
 			}
 		}
 		return playersNotFolded;
 	}
-
-	
-	//start of copied gamestate methods
-	
-	public void river(){
-		betRound();
-		RankHands rank = new RankHands(); 
-		int winner = rank.ranking(tableCards, getNotFoldedPlayers(), notWinners);
-		//System.out.println("The winner is player " +  (winner+1));
-		//Winner is printed as the correct number
-		players.get(winner).addChips(pot);
-		pot = 0;
-		
-	}
-	
+	*/
 	public void betRound(){
 		boolean foo = false;
 		while(!foo){
@@ -350,9 +150,9 @@ public class UI extends Application {
 			Scanner scanner = new Scanner(System.in);
 			int currentTurn = playerTurn % numberOfPlayers;
 			Player currentPlayer = players[currentTurn];
-			int playersLeft = 5;
+			int playersLeft = players.length;
 			
-			for(int x = 0; x < 5; x++)
+			for(int x = 0; x < players.length; x++)
 			{
 				if(players[x].getDidFold())
 					playersLeft--;
@@ -413,5 +213,55 @@ public class UI extends Application {
 		}
 	}
 	
+	public void preflop()
+	{
+		betRound();
+	}
+	public void initialize(){
+		//TODO number of chips as an arguement
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("How many players? ");
+		numberOfPlayers = scanner.nextInt();
+			players = new Player[numberOfPlayers];
+			System.out.println("How many chips would you like to start with?");
+			int startingChips = scanner.nextInt();
+			populateTable(startingChips,numberOfPlayers);
+			System.out.println("\nYOU ARE PLAYER 1");
+			Deck deck = new Deck();
+			deck.deal(players, tableCards);
+			
+			//put into flop ?????
+			
+		while (getPlayersWithChips() > 1){
+			
+			
+			preflop();
+			callAmount = 0;
+			flop();
+			callAmount = 0;
+			turn();
+			callAmount = 0;
+			river();
+			callAmount = 0;
+			getPlayersWithChips();
+			deck.resetDeck();
+			deck.deal(players, tableCards);
+			
+			
+			//Tester above
+		}
 	
+	}
+	
+	// CHANGES : reset call amounts in last while loop lines 236-
+	// getPlayersWithChips method 
+	// reset pot after its won in river method
+	// adding pot to the winning players stack line 112 in river method
+	
+	 
+
+	
+	
+
 }
+
