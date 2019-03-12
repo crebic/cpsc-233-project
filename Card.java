9c43b5efcf5028d7d70969dc862f7ae3ce500156
samRobertson1 <47336846+samRobertson1@users.noreply.github.com>
2019-03-12 13:51:40 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,4 +1,4 @@
-public class Card{
+public class Card implements comparable{
 
 
 	private int value;
@@ -15,6 +15,14 @@ public class Card{
 
 	public int getValue(){
 		return value;
+	}
+	
+	public int compareTo(Object o){
+		if (o instanceOf Card){
+			Card c = (Card) o;
+		} else return 0;//If given an improper input
+		
+		return value - c.getValue();
 	}
 
 }
