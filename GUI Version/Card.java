bc7e19259cefd28ac25aca92c2dd0caf67d48871
Jesse <47336819+crebic@users.noreply.github.com>
2019-03-25 11:52:38 -0600

    Add files via upload

--- GUI Version/Card.java
+++ GUI Version/Card.java
@@ -2,24 +2,41 @@ public class Card implements Comparable {
     private int value;
     private String suit;
 
+    /**
+     * @param v number value of card
+     * @param f suit calue of card
+     */
     public Card(int v, String f) {
         value = v;
         suit = f;
     }
 
+    /**
+     * @param c card
+     */
     public Card(Card c) {
         value = c.getValue();
         suit = c.getSuit();
     }
 
+    /**
+     * @return number value of card
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * @return suit value of card
+     */
     public String getSuit() {
         return suit;
     }
 
+    /**
+     * @param 0 card to compare to
+     * @return difference between values of cards, or 0 if o is not a card
+     */
     public int compareTo(Object o) {
         if (o instanceof Card) {
             Card c = (Card) o;
@@ -28,7 +45,10 @@ public class Card implements Comparable {
         //In the case of incorrect inputs
         else return 0;
     }
-
+    
+    /**
+     * @return string of card (to get the png file)
+     */
     public String toString() {
         //For showing card images
         if (value < 11) {
