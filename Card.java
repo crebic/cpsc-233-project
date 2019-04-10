b6f8fdb33918502e51a7f4ead5f5c0a263ab4f00
Cicada3301q <47336875+Cicada3301q@users.noreply.github.com>
2019-04-10 19:20:10 -0400

    Update Card.java
    Java docc'd

--- Card.java
+++ Card.java
@@ -1,28 +1,51 @@
 import java.io.Serializable;
+//* Authors Sam Robertson 
+//* 2018 February 02
+
 
 public class Card implements Comparable, Serializable {
     private static final long serialVersionUID = 1L;
+    
     private int value;
     private String suit;
 
+    
+    /**
+	*@param v of type int which is the numeric value of the card (jack is 11)
+	*@param s of type String which holds the suite value of a card (Club is c)
+	*/
+    
     public Card(int v, String f) {
         value = v;
         suit = f;
     }
 
+    /**
+	*@param c of type card, copy constructor 
+	*/
     public Card(Card c) {
         value = c.getValue();
         suit = c.getSuit();
     }
-
+    
+    /**
+	*@return value, returns numerical value of a card
+	*/
     public int getValue() {
         return value;
     }
-
+    /**
+	*@return returns the String Suit
+	*/
     public String getSuit() {
         return suit;
     }
-
+    
+     /**
+	*@param o of type Object, this method compares two instances of a card
+    *@return value, returns the difference in the numeric value between two instances of a card 
+    
+	*/
     public int compareTo(Object o) {
         if (o instanceof Card) {
             Card c = (Card) o;
@@ -31,7 +54,10 @@ public class Card implements Comparable, Serializable {
         //In the case of incorrect inputs
         else return 0;
     }
-
+    
+     /**
+	*@return returns a string used as a relative path from the current folder to retrieve the png image of a card. 
+	*/
     public String toString() {
         //For showing card images
         String relativePath = "./CardFolder/";
@@ -48,6 +74,9 @@ public class Card implements Comparable, Serializable {
         }
     }
     
+      /**
+	*@return, returns a string of the card, ie 10 of clubs is 10c.
+	*/
     public String toDisplayString() {
         if (value < 11) {
             return "" + value + suit.charAt(0);
